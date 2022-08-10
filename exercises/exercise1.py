62749c9b5973bcdca28d13685bbf81aed918c042
josedaniele <josedaniele493@gmail.com>
2022-08-10 12:17:09 -0300

    Primer ejercicio resuelto

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -6,10 +6,13 @@ Calcular el área del cuadrado usando las variables disponibles.
 Restricción: Usar el operador de multiplicación
 """
 
+from operator import mod
+
+
 lado_cuadrado = 5
 
 # COMPLETAR - INICIO
-lado_cuadrado = lado_cuadrado * 2
+area_cuadrado = lado_cuadrado * lado_cuadrado
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
@@ -22,7 +25,7 @@ Re-Escribir usando el operador de potencia.
 lado_cuadrado = 5
 
 # COMPLETAR - INICIO
-area_cuadrado = 3
+area_cuadrado = lado_cuadrado**2
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
@@ -35,7 +38,7 @@ Re-Escribir usando la función pow.
 lado_cuadrado = 5
 
 # COMPLETAR - INICIO
-
+area_cuadrado = pow(lado_cuadrado,2)
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
@@ -50,7 +53,7 @@ precio = 3.74
 presupuesto_disponible = 10
 
 # COMPLETAR - INICIO
-
+cantidad_a_comprar = presupuesto_disponible//precio
 # COMPLETAR - FIN
 
 assert cantidad_a_comprar == 2
@@ -64,7 +67,7 @@ Restricción: Usar el operador módulo.
 numero_incalculable = 2 ** 54 - 1
 
 # COMPLETAR - INICIO
-
+es_divisible_por_siete = numero_incalculable % 7
 # COMPLETAR - FIN
 
-assert es_divisible_por_siete
+assert es_divisible_por_siete == 0
